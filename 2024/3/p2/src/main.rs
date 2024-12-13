52ef85b9ee1f29c5fb46c159d7c413369ee8a170
Lucian Chauvin <lucianchauvin@gmail.com>
2024-12-12 20:18:20 -0600

    3.2

--- 2024/3/p2/src/main.rs
+++ 2024/3/p2/src/main.rs
@@ -8,14 +8,19 @@ fn main() -> io::Result<()> {
     let f = File::open("input.txt")?;
     let buf = BufReader::new(f);
 
-    let v = buf.lines()
-        .map(|x| Regex::new(r"mul\((?<a>\d+),(?<b>\d+)\)")
-            .unwrap()
-            .captures_iter(x.ok().unwrap().as_str())
-            .map(|c| c.name("a").unwrap()
-                .as_str().parse::<i32>().unwrap()*c.name("b").unwrap()
-                .as_str().parse::<i32>().unwrap()).reduce(|i,j| i+j))
-        .reduce(|i,j| Some(i.unwrap()+j.unwrap())).unwrap().unwrap();
+    let v = Regex::new(r"mul\((?<a>\d+),(?<b>\d+)\)")
+        .unwrap()
+        .captures_iter(
+            Regex::new(r"don\'t\(\).*?(do\(\)|$)")
+            .unwrap().split(buf.lines().map(|x| x.ok().unwrap())
+                .collect::<Vec<String>>()
+                .join("").as_str())
+            .collect::<Vec<&str>>()
+            .join("").as_str())
+        .map(|c| c.name("a").unwrap()
+            .as_str().parse::<i32>().unwrap()*c.name("b").unwrap()
+            .as_str().parse::<i32>().unwrap()).reduce(|i,j| i+j)
+        .unwrap();
 
     println!("{:#?}", v);
     Ok(())
